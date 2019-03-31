e2dbd8382e32eed385d9fa665eb8f0ec28c3ae1f
yuji <yuji.hamaguchi@uzabase.com>
2019-03-31 09:07:28 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::str::FromStr;
+use std::io::Write;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+
+}
+
+fn gcd(n: u64, m: u64) -> u64 {
+    if n == 0 {
+        m
+    } else {
+        gcd(m % n, n)
+    }
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
